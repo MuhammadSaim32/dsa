29bf0860b6ebd444ed67cef4719e4cfc807c8501
MuhammadSaim32 <muhammad.saim.saleem.3.8@gmail.com>
2026-04-07 00:58:34 +0500

    Added BSF ALog

--- Graph/AdjanceyLIst.cpp
+++ Graph/AdjanceyLIst.cpp
@@ -1,44 +1,77 @@
-#include<iostream>
-#include<list>  
+#include <iostream>
+#include <list>
+#include <queue>
+#include <vector>
 using namespace std;
 
-class Graph{
+class Graph
+{
     int V;
     list<int> *l;
-    public:
-    Graph(int v){
-        this->V=v;
-        l=new list<int>[v];
+
+public:
+    Graph(int v)
+    {
+        this->V = v;
+        l = new list<int>[v];
     }
 
-    void AddEdge(int u ,int v){
+    void AddEdge(int u, int v)
+    {
         l[u].push_back(v);
         l[v].push_back(u);
-
     }
 
-    void PrintEdge(){
-        for(int i=0;i<V;i++){
-            cout<<i<<"->";
-            for(int n:l[i]){
-                cout<<n<<",";
+    void PrintEdge()
+    {
+        for (int i = 0; i < V; i++)
+        {
+            cout << i << "->";
+            for (int n : l[i])
+            {
+                cout << n << ",";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
 
+    void BSF(int src)
+    {
+        vector<bool> vist(V, false);
+        queue<int> q;
+        q.push(src);
+        vist[src] = true;
+
+        while (q.size() != 0)
+        {
+            int u = q.front();
+            q.pop();
+            cout << u << ":";
+            for (int n : l[u])
+            {
+                if (!vist[n])
+                {
+                    q.push(n);
+                    vist[n] = true;
+                }
+            }
+        }
+    }
 };
 
-int main(){
+int main()
+{
 
-Graph g(5);
+    Graph g(5);
 
-g.AddEdge(0,1);
-g.AddEdge(1,2);
-g.AddEdge(1,3);
-g.AddEdge(2,3);
-g.AddEdge(2,4);
+    g.AddEdge(0, 1);
+    g.AddEdge(1, 2);
+    g.AddEdge(1, 3);
+    g.AddEdge(2, 3);
+    g.AddEdge(2, 4);
 
-g.PrintEdge();
+    // g.PrintEdge();
+    g.BSF(0);
+
     return 0;
 }
