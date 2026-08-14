db4d9075195ca2e03cba33cda3afc2849b9d6c01
MuhammadSaim32 <muhammad.saim.saleem.3.8@gmail.com>
2026-08-14 19:48:10 +0500

    binary tree

--- binaryTrees/binaryTree.cpp
+++ binaryTrees/binaryTree.cpp
@@ -1,95 +1,122 @@
-#include<iostream>
-#include<vector>
-#include<queue>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    Node(int value){
-        data=value;
-        left=right=NULL;
+    Node(int value)
+    {
+        data = value;
+        left = right = NULL;
     }
 };
 
+int idx = -1;
 
- int idx=-1;
+Node *BuildTreeFromPreOrder(vector<int> preorder)
+{
 
-Node* BuildTreeFromPreOrder(vector<int> preorder){
+    idx++;
 
-idx++;
+    if (preorder[idx] == -1)
+    {
+        return NULL;
+    }
 
-if(preorder[idx]==-1){
-    return NULL;
-}
-
-    Node* root=new Node(preorder[idx]);
-    root->left=BuildTreeFromPreOrder(preorder);
-    root->right=BuildTreeFromPreOrder(preorder);
+    Node *root = new Node(preorder[idx]);
+    root->left = BuildTreeFromPreOrder(preorder);
+    root->right = BuildTreeFromPreOrder(preorder);
     return root;
 }
 
-void PreorderTravel(Node* node){
-    if(node==NULL) return;
-    cout<<node->data<<" ";
+void PreorderTravel(Node *node)
+{
+    if (node == NULL)
+        return;
+    cout << node->data << " ";
     PreorderTravel(node->left);
     PreorderTravel(node->right);
-
 }
 
-
-void InorderTravel(Node* node){
-    if(node==NULL) return; 
+void InorderTravel(Node *node)
+{
+    if (node == NULL)
+        return;
     InorderTravel(node->left);
-    cout<<node->data<<" ";
+    cout << node->data << " ";
     InorderTravel(node->right);
 }
 
-void postOrderTravel(Node* node){
-    if(node==NULL) return;
+void postOrderTravel(Node *node)
+{
+    if (node == NULL)
+        return;
     postOrderTravel(node->left);
     postOrderTravel(node->right);
-    cout<<node->data<<" ";
+    cout << node->data << " ";
 }
 
-void LevelOrderTravel(Node* node){
-    queue<Node*> q;
+void LevelOrderTravel(Node *node)
+{
+    queue<Node *> q;
     q.push(node);
     q.push(NULL);
-    while(q.size()>0){
-        Node* curr = q.front();q.pop();
-        if(curr==NULL){
-            if(!q.empty()){
-                cout<<"\n";
+    while (q.size() > 0)
+    {
+        Node *curr = q.front();
+        q.pop();
+        if (curr == NULL)
+        {
+            if (!q.empty())
+            {
+                cout << "\n";
                 q.push(NULL);
                 continue;
-            }else{
+            }
+            else
+            {
                 break;
             }
         }
 
-        cout<<curr->data<<" ";
+        cout << curr->data << " ";
 
-
-        if(curr->left!=NULL){
+        if (curr->left != NULL)
+        {
             q.push(curr->left);
         }
 
-         if(curr->right!=NULL){
+        if (curr->right != NULL)
+        {
             q.push(curr->right);
         }
     }
-
 }
 
-int main(){
+void levelOrder(Node *node, int k)
+{
+    if (node == NULL || k < 0)
+        return;
+    k--;
+    if (0 == k)
+    {
+        cout << node->data;
+    }
+    levelOrder(node->left, k);
+    levelOrder(node->right, k);
+}
 
-    vector<int>preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
-    Node* root=BuildTreeFromPreOrder(preorder);
-    PreorderTravel(root);
+int main()
+{
+
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node *root = BuildTreeFromPreOrder(preorder);
+    levelOrder(root, 3);
     return 0;
-
 }
