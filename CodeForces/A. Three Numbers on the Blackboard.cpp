dfc3e0afde94177ba7803fd93d1e22826086735a
MuhammadSaim32 <muhammad.saim.saleem.3.8@gmail.com>
2026-08-14 19:47:12 +0500

    added new prblem

---
+++ CodeForces/A. Three Numbers on the Blackboard.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        vector<int> arr(3);
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> arr[i];
+        }
+
+        sort(arr.begin(), arr.end());
+        if (arr[0] + arr[1] > arr[2])
+        {
+            cout << arr[2] - arr[0]<<endl;
+        }
+        else
+        {
+            arr[2] = arr[0] + arr[1];
+            cout << arr[2] - arr[0]<<endl;
+        }
+    }
+
+    return 0;
+}
